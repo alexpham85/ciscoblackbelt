7190f9de1d04a003370764590049412f64524033
Alex Pham Trung Hieu  (NCS) <trunghieu@ncs.com.sg>
2018-02-06 00:14:00 +0800

    Add existing file

---
+++ Camp1_Day2_assign2.py
@@ -0,0 +1,19 @@
+# Libraries and Functions always come in handy to developers by allowing reusability of existing code. There are certain well known inherent libraries that you have access to after installing python. By using these libraries and functions in them, write a program to guess a randomly generated number between 1 and 10. 
+# For Example: 
+# 
+# Guess the number: 4
+# Wrong, try again! 
+# 
+# Guess the number: 8
+# Correct! 
+# 
+# Hint: Figure out which library the “randint” function belongs to.  
+
+import random
+
+inNum = int(raw_input('Guess the number:'))
+genNum = random.randint(1, 10)
+if (inNum == genNum):
+    print "Correct!"
+else:
+    print "Wrong, try again!"
